5834110606f551ae7164f4c4d747bcca0372412c
bharadwaj-ni <bharadwaj.ni@husky.neu.edu>
2020-04-10 21:30:45 -0400

    1st commit adding business networks

---
+++ CounterfeitMedicationTrackingSystem/src/Business/Network.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+/**
+ *
+ * @author nived
+ */
+public class Network extends Enterprise {
+    
+    private int networkID;
+    private String networkName;
+    private EnterpriseDirectory enterpriseDirectory;
+    
+    public Network(){
+        super("");
+        enterpriseDirectory=new EnterpriseDirectory();
+    }
+
+    public int getNetworkID() {
+        return networkID;
+    }
+
+    public void setNetworkID(int networkID) {
+        this.networkID = networkID;
+    }
+
+    public String getNetworkName() {
+        return networkName;
+    }
+
+    public void setNetworkName(String networkName) {
+        this.networkName = networkName;
+    }
+
+    public EnterpriseDirectory getEnterpriseDirectory() {
+        return enterpriseDirectory;
+    }
+
+    public void setEnterpriseDirectory(EnterpriseDirectory enterpriseDirectory) {
+        this.enterpriseDirectory = enterpriseDirectory;
+    }
+    
+}
