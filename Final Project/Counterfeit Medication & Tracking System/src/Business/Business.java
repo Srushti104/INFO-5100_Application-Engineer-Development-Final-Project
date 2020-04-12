6de6958159198c06fe4917ab0ccf2ad44559b3af
srushti104 <dhamangaonkar.s@husky.edu.neu>
2020-04-11 20:34:41 -0400

    MainJFrame Changes

---
+++ Final Project/Counterfeit Medication & Tracking System/src/Business/Business.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+import Business.Roles.Role;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author srush
+ */
+public class Business extends Enterprise{
+    
+    public static Business business;
+//    private NetworkDirectory networkDirectory;
+
+//    public Business() {
+//        super("Drug Counterfeit System");
+////        networkDirectory=new NetworkDirectory();
+//          }
+    
+     public static Business getInstance()
+    {
+        if(business == null)
+        {
+            business = new Business();
+        }
+        return business;
+    }
+
+   
+
+//    public NetworkDirectory getNetworkDirectory() {
+//        return networkDirectory;
+//    }
+
+    
+//    @Override
+    public ArrayList<Role> getSupportedRoles() {
+       ArrayList<Role> roles = new ArrayList<Role>();
+        // add roles
+        return roles;
+    }
+        
+}
---
+++ Final Project/Counterfeit Medication & Tracking System/src/Business/Enterprise.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+/**
+ *
+ * @author srush
+ */
+public class Enterprise extends Organization{
+    private String enterpriseName;
+    private String enterpriseType;    
+    
+    public static final String MANUFACTURER = "Manufacturer Enterprise";
+    public static final String DISTRIBUTOR = "Distributor Enterprise";
+    
+}
